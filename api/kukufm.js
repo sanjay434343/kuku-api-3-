8957a104567774432dabe47822687d0df964c711
sanjay <sanjay13649@gmail.com>
2025-09-18 14:33:11 +0530

    Update kukufm.js

--- api/kukufm.js
+++ api/kukufm.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
   try {
     const response = await axios.get(url, { params, headers });
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(response.data); // send full response back
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({
       error: "Failed to fetch KukuFM data",
